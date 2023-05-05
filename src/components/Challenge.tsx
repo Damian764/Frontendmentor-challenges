a2196455f4cb91e1d6ea5c08fe34fa0a5be23dc6
Damian764 <damiang764@gmail.com>
2023-05-05 15:34:52 +0200

    Update Challenge.tsx
    --add icons to buttons
    --add raw view button
    --add technologies used section
    --begin to code pseudo browser view

--- src/components/Challenge.tsx
+++ src/components/Challenge.tsx
@@ -1,11 +1,11 @@
 import { Link, useLoaderData } from "react-router-dom";
+import { MdKeyboardArrowLeft, MdOpenInFull } from "react-icons/md";
+
+
 import { ChallengeListStructure, getChallenge } from "../data/challenges/challenges_data"
 import './scss/_challenge.scss'
 
-interface loaderParams {
-    challengeId: string
-}
-export function loader({ params }: { params: loaderParams }) {
+export function loader({ params }: any) {
     const challenge = getChallenge(params.challengeId);
     return challenge
 }
@@ -14,11 +14,33 @@ const Challenge = () => {
     return (
         <div className="challenge container">
             <div className="challenge-header">
-                <Link to={'/challenges'}>Back to challenges list</Link>
+                <Link className="go-back" to={'/challenges'}><MdKeyboardArrowLeft />Back to challenges list</Link>
                 <div className="title">{challenge.title}</div>
+                <Link className="view-raw" to={'raw'}>View full page <MdOpenInFull /></Link>
             </div>
+            {
+                challenge.technologies &&
+                <div className="technologies">
+                    <h2>Technologies used for this project</h2>
+                    <ul className='technologies-list'>
+                        {challenge.technologies.map(technology =>
+                            <li className={`technologies-item technologies-${technology.replace(/\s+/g, '-').toLowerCase()}`}>{technology}</li>
+                        )}
+                    </ul>
+                </div>
+            }
             <section className="content">
-                {challenge.element}
+                <div className="top-bar">
+                    <div className="dots">
+                        <div className="dot"></div>
+                        <div className="dot"></div>
+                        <div className="dot"></div>
+                    </div>
+                    <div className="tab">{challenge.title}</div>
+                </div>
+                <div className="body">
+                    {challenge.element}
+                </div>
             </section>
         </div>
     )
